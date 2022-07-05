03fb26707948a2e2cba04619d3fe389c6be9a649
Naoki Ikeguchi <me@s6n.jp>
2022-07-05 23:24:52 +0900

    feat(apdu-core): Include response in Error

--- apdu-core/src/error.rs
+++ apdu-core/src/error.rs
@@ -1,18 +1,21 @@
 use std::fmt::{Display, Formatter};
 
+use crate::Response;
+
 /// An error that was returned from the card or reader
 #[derive(Debug)]
 pub struct Error {
-    pub sw1: u8,
-    pub sw2: u8,
+    pub response: Response,
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let (sw1, sw2) = self.response.trailer;
+
         write!(
             f,
             "The APDU reader returned an error ({:#X}, {:#X}).",
-            self.sw1, self.sw2
+            sw1, sw2,
         )
     }
 }
--- apdu-core/src/response.rs
+++ apdu-core/src/response.rs
@@ -1,7 +1,7 @@
 use crate::Error;
 
 /// An response that was received from the card
-#[derive(Default)]
+#[derive(Debug, Default)]
 pub struct Response {
     pub payload: Vec<u8>,
     pub trailer: (u8, u8),
@@ -38,17 +38,10 @@ impl From<Response> for Result<Vec<u8>, Error> {
     /// Converts the response to a result of octets.
     fn from(response: Response) -> Self {
         let is_ok = response.is_ok();
-        let Response { payload, trailer } = response;
 
         match is_ok {
-            true => Ok(payload),
-            _ => Err(trailer.into()),
+            true => Ok(response.payload),
+            _ => Err(Error { response }),
         }
-    }
-}
-
-impl From<(u8, u8)> for Error {
-    fn from((sw1, sw2): (u8, u8)) -> Self {
-        Error { sw1, sw2 }
     }
 }
